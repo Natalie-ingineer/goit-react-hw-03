4d6e00ba3a20ed7d70bd49fa35580754b142cfdd
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-01-25 12:42:15 +0200

    update step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,14 @@ import { useState, useEffect } from "react";
 export const App = () => {
   const [nameFilter, setNameFilter] = useState("");
   const [users, setUsers] = useState([
-    // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    "Jacob",
-    "Mango",
-    "Elena",
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ]);
 
   const visibleUsers = users.filter((user) =>
-    user.toLowerCase().includes(nameFilter.toLowerCase())
+    user.name.toLowerCase().includes(nameFilter.toLowerCase())
   );
   console.log(visibleUsers);
 
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,7 +5,11 @@ export const ContactList = ({ items }) => {
   return (
     <ul>
       {items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>
+          <p>{item.name}</p>
+          <p>{item.number}</p>
+          <button>Delete</button>
+        </li>
       ))}
     </ul>
   );
