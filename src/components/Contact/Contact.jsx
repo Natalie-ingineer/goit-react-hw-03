ce910ae81593d70b8386cdd528102fe69366654e
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-01-26 09:38:59 +0200

    style card

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,10 +2,19 @@ import css from "./Contact.module.css";
 
 export const Contact = ({ contact, onDeleteUser }) => {
   return (
-    <div>
-      <p>{contact.name}</p>
-      <p>{contact.number}</p>
-      <button onClick={() => onDeleteUser(contact.id)}>Delete</button>
+    <div className={css.wraper}>
+      <div>
+        <p className={css.userName}>{contact.name}</p>
+        <p className={css.userPhone}>{contact.number}</p>
+      </div>
+      <div>
+        <button
+          className={css.btnCard}
+          onClick={() => onDeleteUser(contact.id)}
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
