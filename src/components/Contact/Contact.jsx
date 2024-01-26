742abbf72cccac6766d6eb3aa98e886a782e8c2c
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-01-26 16:03:04 +0200

    update style card

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,20 @@
+import { BsPersonLinesFill } from "react-icons/bs";
+import { GiRotaryPhone } from "react-icons/gi";
 import css from "./Contact.module.css";
 
 export const Contact = ({ contact, onDeleteUser }) => {
   return (
     <div className={css.wraper}>
-      <div>
-        <p className={css.userName}>{contact.name}</p>
-        <p className={css.userPhone}>{contact.number}</p>
+      <div className={css.infoWrap}>
+        <p className={css.userName}>
+          <BsPersonLinesFill className={css.icon} /> {contact.name}
+        </p>
+        <p className={css.userPhone}>
+          <GiRotaryPhone className={css.icon} /> {contact.number}
+        </p>
       </div>
       <div>
-        <button
-          className={css.btnCard}
-          onClick={() => onDeleteUser(contact.id)}
-        >
+        <button className={css.button} onClick={() => onDeleteUser(contact.id)}>
           Delete
         </button>
       </div>
